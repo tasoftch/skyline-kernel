b2dd402d51cf16ddf2a16954b503858e9376cc53
Th. Abplanalp <info@tasoft.ch>
2021-01-07 12:42:30 +0100

    FIX: Root and User dir

--- src/Service/Manager/SkyServiceManager.php
+++ src/Service/Manager/SkyServiceManager.php
@@ -3,14 +3,12 @@
 
 namespace Skyline\Kernel\Service\Manager;
 
-
-use Skyline\Kernel\Bootstrap;
 use TASoft\Service\ServiceManager;
 
 class SkyServiceManager extends ServiceManager
 {
 	public function getRegisteredServicePersistentFile(): string
 	{
-		return Bootstrap::$skylineDirectory . "service.management.php";
+		return SkyGetPath("$(U)/service.management.php");
 	}
 }
